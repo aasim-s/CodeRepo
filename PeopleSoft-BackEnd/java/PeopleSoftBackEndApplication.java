d23a30052017529ece1f999228ca34055aeb509c
aasim-s <126641322+aasim-s@users.noreply.github.com>
2023-03-10 00:15:44 +0530

    initial structure and get Method
    Backend for Employee management web application PeopleSoft

---
+++ PeopleSoft-BackEnd/java/PeopleSoftBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.peopleSoft.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleSoftBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleSoftBackEndApplication.class, args);
+	}
+
+}
